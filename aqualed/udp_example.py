f0a5d6fc701988916cc833a9dca59f3cdd468177
kattenekis <kattenekis@email.github.com>
2017-02-16 12:43:24 +0100

    Simulated time (very fast) to examine good color combinations. Seem OK - next step to use real time instead.

--- aqualed/udp_example.py
+++ aqualed/udp_example.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 import time
-
+import datetime
 import rgbwtranslation
 
 try:
@@ -17,24 +17,20 @@ timestart = time.time()
 current_time = 0
 
 while 1 :
-    time.sleep(1)
+    time.sleep(0.25)
+    current_time += 0.1
+    if current_time >=24:
+        current_time = 0
+    print('Time of day: {:.2f} [hour]'.format(current_time))
     try :
-        # msg = input('Enter message to send : ')
-        msg = '1:1:2:255'
-        # print(msg)
-        # print(time.time()-timestart)
-        current_time = time.time() - timestart
         rgbw = rgbwtranslation.get_rgbw(current_time,1024)
         outstr = str(rgbw[0]) + ':' + str(rgbw[1]) + ':' + str(rgbw[2]) + ':' + str(rgbw[3])
-        print('RGBW: ' + outstr)
-
-        #Set the whole string
+        print('   RGBW string: ' + outstr)
+        #Send the whole string
         s.sendto(outstr.encode('utf-8'), (host, port))
     # Some problem sending data ??
     except socket.error as e:
         print ('Error Code : ' + str(e[0]) + ' Message ' + e[1])
         sys.exit()
-
-
 
 print('Program Complete')
