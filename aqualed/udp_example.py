5e78b3f5c175c861075b4a62de51399aed4fa61b
andreas <kattenekis@katten-github.se>
2017-04-21 09:37:30 +0200

    Debug commit! Updated code with:  Linear approximation for smoothe transitions between colour values  New colour / time scheme

--- aqualed/udp_example.py
+++ aqualed/udp_example.py
@@ -20,15 +20,16 @@ while 1 :
     time.sleep(0.25)
 
     # code to fake time - debugging
-    #current_time += 0.1
-    #if current_time >=24:
-    #    current_time = 0
+
+    current_time += 0.1
+    if current_time >=24:
+        current_time = 0
 
     # code for real time use:
-    now =  datetime.datetime.now()
-    current_time = float(now.hour)+float(now.minute)/60
+    # now =  datetime.datetime.now()
+    # current_time = float(now.hour)+float(now.minute)/60
 
-    print('Time of day: {:.2f} [hour]'.format(current_time))
+    print('Time of day: {:.2f} [hour]'.format(current_time), end=" ")
     try :
         rgbw = rgbwtranslation.get_rgbw(current_time,1024)
         outstr = str(rgbw[0]) + ':' + str(rgbw[1]) + ':' + str(rgbw[2]) + ':' + str(rgbw[3])
